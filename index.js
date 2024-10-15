955f00fe5c67de70c0cc204f2cd49a54b60c67a3
Lucas <lucas.2020109662@unicap.br>
2024-10-14 23:27:57 -0300

    Finish gear

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const volume = document.getElementById("volume");
-function audioPlayer(volume, onClick){
-    
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const wikiLinks = document.querySelectorAll('.wiki-link');
+
+    wikiLinks.forEach(link => {
+        link.addEventListener('mouseover', function() {
+            const imageUrl = this.getAttribute('data-image-url');
+            this.style.setProperty('--image-url', `url(${imageUrl})`);
+        });
+
+        link.addEventListener('click', function() {
+            const wikiUrl = this.getAttribute('data-wiki-url');
+            window.open(wikiUrl, '_blank');
+        });
+    });
+});
